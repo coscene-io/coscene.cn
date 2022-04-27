6b51176e956d2421993a045c9624fc948ad058e1
Yujing Zheng <yujingz@me.com>
2022-04-27 21:56:06 +0800

    update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,25 +3,13 @@ import React from 'react';
 const Footer = () => (
   <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
     <div className="flex -mx-3">
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">About Us</h2>
-        All Rights R
-      </div>
+      <div className="flex-1 px-3">© {new Date().getFullYear()} coScene Inc.</div>
 
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Important Links</h2>
+        <h2 className="text-lg font-semibold">Connect with us</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://terms.com">Terms &amp; Conditions</a>
-          </li>
-        </ul>
-      </div>
-
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Social Media</h2>
-        <ul className="mt-4 leading-loose">
-          <li>
-            <a href="https://twitter.com/HuntaroSan">Twitter</a>
+            <a href="https://github.com/coscene-io">GitHub</a>
           </li>
         </ul>
       </div>
