1a45ff7e988942e8a1812f7fcd30172c5eedde50
Yujing Zheng <yujing@coscene.io>
2022-12-09 21:36:54 +0800

    pam anderson is going to love me.

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
         tailwindConfig: `tailwind.config.js`,
       },
     },
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
