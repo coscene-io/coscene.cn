451147c2e0cd51517b554a784448103d50a3f37a
Yujing Zheng <yujing@coscene.io>
2022-12-09 20:31:33 +0800

    Don’t mess with Voodoo

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Footer from './Footer';
 import Header from './Header';
-import { Helmet } from "react-helmet"
+import { Helmet } from 'react-helmet';
 
 const Layout = ({ children }) => {
   return (
@@ -9,7 +9,22 @@ const Layout = ({ children }) => {
       <Helmet>
         <meta charSet="utf-8" />
         <title>coScene 刻行</title>
-        <link rel="canonical" href="http://mysite.com/example" />
+        <link rel="canonical" href="http://www.coscene.cn" />
+
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta
+          name="description"
+          content="场景数据爆炸式增长的当下和未来，刻行帮助各大企业解决增长难题，大幅度降低应用、研发、运维、和售后的成本，降低行业门槛，推动行业发展。"
+        />
+        <meta
+          property="og:title"
+          content="刻行，新一代场景数据平台，让你的场景数据应用、开发更高效 | 刻行，场景数据应用平台"
+        />
+        <meta
+          property="og:description"
+          content="场景数据爆炸式增长的当下和未来，刻行帮助各大企业解决增长难题，大幅度降低应用、研发、运维、和售后的成本，降低行业门槛，推动行业发展。"
+        />
       </Helmet>
       <Header />
       <main className="text-gray-900">{children}</main>
